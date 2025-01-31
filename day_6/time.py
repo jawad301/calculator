20e44159eef3db6c67a693e57840a90ab5e2c61d
Jawad Ahmad <hafizjawad3311@gmail.com>
2025-01-31 20:27:45 +0500

    work

--- day_6/time.py
+++ day_6/time.py
@@ -1,2 +1,7 @@
-b = "hello how are you"
-print ( b [3:])
+temp = int(input("Enter temp in Celsius: "))
+if temp <=15:
+    print("It's cold outside.")
+elif temp >=30:
+    print("It's warm outside.")
+else :
+    print("It's nice outside.")
