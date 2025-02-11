b6f691d66f4d0f57d9167f882a20d9dec5a2fa42
Jawad Ahmad <hafizjawad3311@gmail.com>
2025-02-11 17:40:51 +0500

    new code

--- set-code/factorial.py
+++ set-code/factorial.py
@@ -1,5 +1,5 @@
 def find_factorial (num) : 
     return 1 if num == 0 else num * find_factorial(num - 1) 
 user = int(input("Enter a number: "))
-find_factorial(user)
-print ( "The factorial of the Number is:")
+result = find_factorial(user)
+print ( "The factorial of the Number is:",result)
